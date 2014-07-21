bbbf822162f128be84296ed240409a73e2f86da9
Justin Manley <manleyjster@gmail.com>
2014-07-21 15:25:15 -0500

    Fix test coverage reporting after test reorganization.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -44,8 +44,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/*.js': 'coverage',
-      'src/*/*.js': 'coverage'
+      '../src/*.js': 'coverage',
+      '../src/*/*.js': 'coverage'
     },
 
 
