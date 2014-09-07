383474c4a4c3ca731ed3f0a809f35d5f1d5e66a8
Justin Manley <manleyjster@gmail.com>
2014-09-07 16:06:14 -0500

    Add tests for L.Illustrate.Textbox#setSize.

--- test/core/TextboxSpec.js
+++ test/core/TextboxSpec.js
@@ -16,13 +16,66 @@ describe("L.Illustrate.Textbox", function() {
 			size = new L.Point(240, 155);
 
 		textbox = new L.Illustrate.Textbox(center, L.Illustrate.Create.Textbox.prototype.options.shapeOptions)
-			.setSize(size);
-		textbox.addTo(map);
+			.setSize(size)
+			.addTo(map);
+	});
+
+	describe("#initialize", function() {
+		it("Should have a _minSize.", function() {
+			expect(textbox).to.have.property("_minSize");
+			expect(textbox._minSize).to.be.an.instanceof(L.Point);
+		});
 	});
 
 	describe("#getContent", function() {
 		it("Should return the empty string for a new textbox with no default value.", function() {
 			expect(textbox.getContent()).to.equal('');
+		});
+	});
+
+	describe("#setSize", function() {
+		it("Should leave _minSize unchanged if called with a size larger than _minSize.", function() {
+			var minSize = textbox._minSize,
+				newSize = minSize.add(new L.Point(100, 100));
+
+			textbox.setSize(newSize);
+
+			expect(textbox._minSize.x).to.equal(minSize.x);
+			expect(textbox._minSize.y).to.equal(minSize.y);
+		});
+
+		it("Should reset _minSize.x if size.x < _minSize.x", function() {
+			var	minSize  = textbox._minSize,
+				newSize  = minSize.add(new L.Point(-100, 100));
+
+			textbox.setSize(newSize);
+
+			expect(textbox._minSize.x).to.equal(newSize.x);
+			expect(textbox._minSize.y).to.equal(minSize.y);
+
+			expect(textbox.getSize()).to.deep.equal(newSize);
+
+		});
+
+		it("Should reset _minSize.x if size.x < _minSize.x", function() {
+			var minSize = textbox._minSize,
+				newSize = minSize.add(new L.Point(100, -100));
+
+			textbox.setSize(newSize);
+
+			expect(textbox._minSize.x).to.equal(minSize.x);
+			expect(textbox._minSize.y).to.equal(newSize.y);
+
+			expect(textbox.getSize()).to.deep.equal(newSize);
+		});
+
+		it("Should reset _minSize if both components are less than _minSize", function() {
+			var	minSize  = textbox._minSize,
+				newSize  = minSize.add(new L.Point(-100, -100));
+
+			textbox.setSize(newSize);
+			expect(textbox._minSize).to.deep.equal(newSize);
+			expect(textbox.getSize()).to.deep.equal(newSize);
 		});
 	});
 
