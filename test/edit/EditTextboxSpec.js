ca3e60fe0beb775ed3a5ec9b723d0e0c4db820c3
Justin Manley <manleyjster@gmail.com>
2014-09-07 16:07:16 -0500

    Add tests for L.Illustrate.ResizeHandle#_onHandleDrag to make sure that the textbox resizes correctly in editing mode.

---
+++ test/edit/EditTextboxSpec.js
@@ -0,0 +1,33 @@
+var expect = chai.expect;
+
+describe("L.Illustrate.Edit.Pointer", function() {
+	var map, textbox;
+
+	beforeEach(function() {
+		var center = new L.LatLng(41.79187262698525, -87.60107517242432),
+			size = new L.Point(240, 155);
+
+		map = L.map(document.createElement('div')).setView([41.7896,-87.5996], 15);
+
+		textbox = new L.Illustrate.Textbox(center, L.Illustrate.Create.Textbox.prototype.options.shapeOptions)
+			.setSize(size)
+			.addTo(map);
+	});
+
+	beforeEach(function() {
+		textbox.editing.enable();
+	});
+
+	describe("#_onHandleDrag", function() {
+		it("Should not change the textbox dimensions when called with initial latlng.", function() {
+			var handle = textbox.editing._resizeHandles[0],
+				size = textbox.getSize();
+
+			handle._onHandleDrag({ target: handle });
+
+			/* textbox.getSize() is equal to 2*this._minSize */
+			expect(textbox.getSize()).to.be.closeToPoint(size);
+		});
+	});
+
+});
